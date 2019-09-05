beb32c2269cf83de7f1d0c0926b824aafae06c0c
YaserAlnajjar <kasseryasser@windowslive.com>
2019-09-05 13:10:55 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,9 +1,9 @@
 let articles = [
     {
         id: 1,
-        title: 'هنا ينتهي حقك في التعلم؟',
+        title: 'ما هو هدفك من البرمجة؟',
         pic: 'https://source.unsplash.com/random/300x300?sig=1',
-        link: 'https://forums.coretabs.net/t/هنا-ينتهي-حقك-في-التعلم؟/2857',
+        link: 'https://forums.coretabs.net/t/ما-هو-هدفك-من-البرمجة؟/2927/4',
         author: {
             name: 'Makhloufi Smail',
             profile_url: 'https://forums.coretabs.net/u/makhlouf',
@@ -12,63 +12,19 @@ let articles = [
     },
     {
         id: 2,
-        title: 'أنظمة التعرف على الكلام',
+        title: '5 قصص نجاح ستبين لك أن الوقت ليس متأخرا أبدا',
         pic: 'https://source.unsplash.com/random/300x300?sig=2',
-        link: 'https://forums.coretabs.net/t/أنظمة-التعرف-على-الكلام/2821',
+        link: 'https://forums.coretabs.net/t/5-قصص-نجاح-ستبين-لك-أن-الوقت-ليس-متأخرا-أبدا-من-أجل-بداية-جديدة/1785',
         author: {
-            name: 'Makhloufi Smail',
-            profile_url: 'https://forums.coretabs.net/u/makhlouf',
-            avatar_url: 'https://forums.coretabs.net/user_avatar/forums.coretabs.net/makhlouf/45/1545_2.png'
+            name: 'Hichem Ahmed',
+            profile_url: 'https://forums.coretabs.net/u/hichem2h',
+            avatar_url: 'https://forums.coretabs.net/user_avatar/forums.coretabs.net/hichem2h/45/2008_2.png'
         }
     },
     {
         id: 3,
-        title: 'أودو، إطار عمل رائع',
-        pic: 'https://source.unsplash.com/random/300x300?sig=3',
-        link: 'https://forums.coretabs.net/t/%D8%A3%D9%88%D8%AF%D9%88%D8%8C-%D8%A5%D8%B7%D8%A7%D8%B1-%D8%B9%D9%85%D9%84-%D8%B1%D8%A7%D8%A6%D8%B9/2816',
-        author: {
-            name: 'Makhloufi Smail',
-            profile_url: 'https://forums.coretabs.net/u/makhlouf',
-            avatar_url: 'https://forums.coretabs.net/user_avatar/forums.coretabs.net/makhlouf/45/1545_2.png'
-        }
-    },
-    {
-        id: 4,
-        title: 'زيادة فرصك كمطور: مشاريعك في البروفايل',
-        pic: 'https://source.unsplash.com/random/300x300?sig=4',
-        link: 'https://forums.coretabs.net/t/%D8%B2%D9%8A%D8%A7%D8%AF%D8%A9-%D9%81%D8%B1%D8%B5%D9%83-%D9%83%D9%85%D8%B7%D9%88%D8%B1-%D9%85%D8%B4%D8%A7%D8%B1%D9%8A%D8%B9%D9%83-%D9%81%D9%8A-%D8%A7%D9%84%D8%A8%D8%B1%D9%88%D9%81%D8%A7%D9%8A%D9%84/2485',
-        author: {
-            name: 'Yaser AlNajjar',
-            profile_url: 'https://forums.coretabs.net/u/YaserAlnajjar',
-            avatar_url: 'https://forums.coretabs.net/user_avatar/forums.coretabs.net/yaseralnajjar/45/1862_2.png'
-        }
-    },
-    {
-        id: 5,
-        title: 'العملاء والعمل الحر',
-        pic: 'https://source.unsplash.com/random/300x300?sig=5',
-        link: 'https://forums.coretabs.net/t/%D8%A7%D9%84%D8%B9%D9%85%D9%84%D8%A7%D8%A1-%D9%88%D8%A7%D9%84%D8%B9%D9%85%D9%84-%D8%A7%D9%84%D8%AD%D8%B1/2789',
-        author: {
-            name: 'Hussein Arasi',
-            profile_url: 'https://forums.coretabs.net/u/Hussien',
-            avatar_url: 'https://forums.coretabs.net/user_avatar/forums.coretabs.net/hussien/45/1525_2.png'
-        }
-    },
-    {
-        id: 6,
-        title: 'خطوتك الاولى لبناء مشروعك',
-        pic: 'https://source.unsplash.com/random/300x300?sig=6',
-        link: 'https://forums.coretabs.net/t/%D8%AE%D8%B7%D9%88%D8%AA%D9%83-%D8%A7%D9%84%D8%A7%D9%88%D9%84%D9%89-%D9%84%D8%A8%D9%86%D8%A7%D8%A1-%D9%85%D8%B4%D8%B1%D9%88%D8%B9%D9%83/2733',
-        author: {
-            name: 'Malek Salem',
-            profile_url: 'https://forums.coretabs.net/u/malek.salem',
-            avatar_url: 'https://forums.coretabs.net/user_avatar/forums.coretabs.net/malek.salem/45/1764_2.png'
-        }
-    },
-    {
-        id: 7,
         title: 'كيف قمت ببناء متجري الالكتروني بجانقو',
-        pic: 'https://source.unsplash.com/random/300x300?sig=7',
+        pic: 'https://source.unsplash.com/random/300x300?sig=3',
         link: 'https://forums.coretabs.net/t/%D9%85%D8%AA%D8%AC%D8%B1%D9%8A-%D8%A7%D9%84%D8%A7%D9%88%D9%84/2740',
         author: {
             name: 'Asmaa Salih',
@@ -77,25 +33,69 @@ let articles = [
         }
     },
     {
-        id: 8,
-        title: 'كيف تصبح المبرمج العبقري المطلوب في كل الشركات',
-        pic: 'https://source.unsplash.com/random/300x300?sig=8',
-        link: 'https://forums.coretabs.net/t/%D9%83%D9%8A%D9%81-%D8%AA%D8%B5%D8%A8%D8%AD-%D8%A7%D9%84%D9%85%D8%A8%D8%B1%D9%85%D8%AC-%D8%A7%D9%84%D8%B9%D8%A8%D9%82%D8%B1%D9%8A-%D8%A7%D9%84%D9%85%D8%B7%D9%84%D9%88%D8%A8-%D9%81%D9%8A-%D9%83%D9%84-%D8%A7%D9%84%D8%B4%D8%B1%D9%83%D8%A7%D8%AA/2714',
+        id: 4,
+        title: 'كيف تقوم بعمل Template للمواقع',
+        pic: 'https://source.unsplash.com/random/300x300?sig=4',
+        link: 'https://forums.coretabs.net/t/سلسلة-template/2916',
         author: {
-            name: 'Hichem Ahmed',
-            profile_url: 'https://forums.coretabs.net/u/hichem2h',
-            avatar_url: 'https://forums.coretabs.net/user_avatar/forums.coretabs.net/hichem2h/45/2008_2.png'
+            name: 'Tarek Amro',
+            profile_url: 'https://forums.coretabs.net/u/tarekamro',
+            avatar_url: 'https://forums.coretabs.net/user_avatar/forums.coretabs.net/tarekamro/120/1991_2.png'
         }
     },
     {
-        id: 9,
-        title: 'التطوع في مواقع الفريلانسر وموقع الامم المتحدة',
-        pic: 'https://source.unsplash.com/random/300x300?sig=9',
-        link: 'https://forums.coretabs.net/t/%D8%A7%D9%84%D8%AA%D8%B7%D9%88%D8%B9-%D9%81%D9%8A-%D9%85%D9%88%D8%A7%D9%82%D8%B9-%D8%A7%D9%84%D9%81%D8%B1%D9%8A%D9%84%D8%A7%D9%86%D8%B3%D8%B1-%D9%88%D9%85%D9%88%D9%82%D8%B9-%D8%A7%D9%84%D8%A7%D9%85%D9%85-%D8%A7%D9%84%D9%85%D8%AA%D8%AD%D8%AF%D8%A9/2737',
+        id: 5,
+        title: 'أنظمة التعرف على الكلام',
+        pic: 'https://source.unsplash.com/random/300x300?sig=5',
+        link: 'https://forums.coretabs.net/t/أنظمة-التعرف-على-الكلام/2821',
+        author: {
+            name: 'Makhloufi Smail',
+            profile_url: 'https://forums.coretabs.net/u/makhlouf',
+            avatar_url: 'https://forums.coretabs.net/user_avatar/forums.coretabs.net/makhlouf/45/1545_2.png'
+        }
+    },
+    {
+        id: 6,
+        title: 'أودو، إطار عمل رائع',
+        pic: 'https://source.unsplash.com/random/300x300?sig=6',
+        link: 'https://forums.coretabs.net/t/%D8%A3%D9%88%D8%AF%D9%88%D8%8C-%D8%A5%D8%B7%D8%A7%D8%B1-%D8%B9%D9%85%D9%84-%D8%B1%D8%A7%D8%A6%D8%B9/2816',
+        author: {
+            name: 'Makhloufi Smail',
+            profile_url: 'https://forums.coretabs.net/u/makhlouf',
+            avatar_url: 'https://forums.coretabs.net/user_avatar/forums.coretabs.net/makhlouf/45/1545_2.png'
+        }
+    },
+    {
+        id: 7,
+        title: '4 إطارات عمل frameworks يجب عليك معرفتها في 2019',
+        pic: 'https://source.unsplash.com/random/300x300?sig=7',
+        link: 'https://forums.coretabs.net/t/4-إطارات-عمل-frameworks-يجب-عليك-معرفتها-في-2019/1663',
+        author: {
+            name: 'Yaser AlNajjar',
+            profile_url: 'https://forums.coretabs.net/u/YaserAlnajjar',
+            avatar_url: 'https://forums.coretabs.net/user_avatar/forums.coretabs.net/yaseralnajjar/45/1862_2.png'
+        }
+    },
+    {
+        id: 8,
+        title: 'العملاء والعمل الحر',
+        pic: 'https://source.unsplash.com/random/300x300?sig=8',
+        link: 'https://forums.coretabs.net/t/%D8%A7%D9%84%D8%B9%D9%85%D9%84%D8%A7%D8%A1-%D9%88%D8%A7%D9%84%D8%B9%D9%85%D9%84-%D8%A7%D9%84%D8%AD%D8%B1/2789',
         author: {
             name: 'Hussein Arasi',
             profile_url: 'https://forums.coretabs.net/u/Hussien',
             avatar_url: 'https://forums.coretabs.net/user_avatar/forums.coretabs.net/hussien/45/1525_2.png'
+        }
+    },
+    {
+        id: 9,
+        title: 'خطوتك الاولى لبناء مشروعك',
+        pic: 'https://source.unsplash.com/random/300x300?sig=9',
+        link: 'https://forums.coretabs.net/t/%D8%AE%D8%B7%D9%88%D8%AA%D9%83-%D8%A7%D9%84%D8%A7%D9%88%D9%84%D9%89-%D9%84%D8%A8%D9%86%D8%A7%D8%A1-%D9%85%D8%B4%D8%B1%D9%88%D8%B9%D9%83/2733',
+        author: {
+            name: 'Malek Salem',
+            profile_url: 'https://forums.coretabs.net/u/malek.salem',
+            avatar_url: 'https://forums.coretabs.net/user_avatar/forums.coretabs.net/malek.salem/45/1764_2.png'
         }
     },
 ];

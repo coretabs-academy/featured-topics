b576e17f24cdcc6314c24566255d1b204ba18848
Ayman Alrifai <ayman97rf@gmail.com>
2019-09-05 19:23:36 +0200

    Update index.js
    Filter random images to just abstract images

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ let articles = [
     {
         id: 1,
         title: 'ما هو هدفك من البرمجة؟',
-        pic: 'https://source.unsplash.com/random/300x300?sig=1',
+        pic: 'https://source.unsplash.com/random/300x300?sig=1&abstract',
         link: 'https://forums.coretabs.net/t/ما-هو-هدفك-من-البرمجة؟/2927/4',
         author: {
             name: 'Makhloufi Smail',
@@ -13,7 +13,7 @@ let articles = [
     {
         id: 2,
         title: '5 قصص نجاح ستبين لك أن الوقت ليس متأخرا أبدا',
-        pic: 'https://source.unsplash.com/random/300x300?sig=2',
+        pic: 'https://source.unsplash.com/random/300x300?sig=2&abstract',
         link: 'https://forums.coretabs.net/t/5-قصص-نجاح-ستبين-لك-أن-الوقت-ليس-متأخرا-أبدا-من-أجل-بداية-جديدة/1785',
         author: {
             name: 'Hichem Ahmed',
@@ -24,7 +24,7 @@ let articles = [
     {
         id: 3,
         title: 'كيف قمت ببناء متجري الالكتروني بجانقو',
-        pic: 'https://source.unsplash.com/random/300x300?sig=3',
+        pic: 'https://source.unsplash.com/random/300x300?sig=3&abstract',
         link: 'https://forums.coretabs.net/t/%D9%85%D8%AA%D8%AC%D8%B1%D9%8A-%D8%A7%D9%84%D8%A7%D9%88%D9%84/2740',
         author: {
             name: 'Asmaa Salih',
@@ -35,7 +35,7 @@ let articles = [
     {
         id: 4,
         title: 'كيف تقوم بعمل Template للمواقع',
-        pic: 'https://source.unsplash.com/random/300x300?sig=4',
+        pic: 'https://source.unsplash.com/random/300x300?sig=4&abstract',
         link: 'https://forums.coretabs.net/t/سلسلة-template/2916',
         author: {
             name: 'Tarek Amro',
@@ -46,7 +46,7 @@ let articles = [
     {
         id: 5,
         title: 'أنظمة التعرف على الكلام',
-        pic: 'https://source.unsplash.com/random/300x300?sig=5',
+        pic: 'https://source.unsplash.com/random/300x300?sig=5&abstract',
         link: 'https://forums.coretabs.net/t/أنظمة-التعرف-على-الكلام/2821',
         author: {
             name: 'Makhloufi Smail',
@@ -57,7 +57,7 @@ let articles = [
     {
         id: 6,
         title: 'أودو، إطار عمل رائع',
-        pic: 'https://source.unsplash.com/random/300x300?sig=6',
+        pic: 'https://source.unsplash.com/random/300x300?sig=6&abstract',
         link: 'https://forums.coretabs.net/t/%D8%A3%D9%88%D8%AF%D9%88%D8%8C-%D8%A5%D8%B7%D8%A7%D8%B1-%D8%B9%D9%85%D9%84-%D8%B1%D8%A7%D8%A6%D8%B9/2816',
         author: {
             name: 'Makhloufi Smail',
@@ -68,7 +68,7 @@ let articles = [
     {
         id: 7,
         title: '4 إطارات عمل frameworks يجب عليك معرفتها في 2019',
-        pic: 'https://source.unsplash.com/random/300x300?sig=7',
+        pic: 'https://source.unsplash.com/random/300x300?sig=7&abstract',
         link: 'https://forums.coretabs.net/t/4-إطارات-عمل-frameworks-يجب-عليك-معرفتها-في-2019/1663',
         author: {
             name: 'Yaser AlNajjar',
@@ -79,7 +79,7 @@ let articles = [
     {
         id: 8,
         title: 'العملاء والعمل الحر',
-        pic: 'https://source.unsplash.com/random/300x300?sig=8',
+        pic: 'https://source.unsplash.com/random/300x300?sig=8&abstract',
         link: 'https://forums.coretabs.net/t/%D8%A7%D9%84%D8%B9%D9%85%D9%84%D8%A7%D8%A1-%D9%88%D8%A7%D9%84%D8%B9%D9%85%D9%84-%D8%A7%D9%84%D8%AD%D8%B1/2789',
         author: {
             name: 'Hussein Arasi',
@@ -90,7 +90,7 @@ let articles = [
     {
         id: 9,
         title: 'خطوتك الاولى لبناء مشروعك',
-        pic: 'https://source.unsplash.com/random/300x300?sig=9',
+        pic: 'https://source.unsplash.com/random/300x300?sig=9&abstract',
         link: 'https://forums.coretabs.net/t/%D8%AE%D8%B7%D9%88%D8%AA%D9%83-%D8%A7%D9%84%D8%A7%D9%88%D9%84%D9%89-%D9%84%D8%A8%D9%86%D8%A7%D8%A1-%D9%85%D8%B4%D8%B1%D9%88%D8%B9%D9%83/2733',
         author: {
             name: 'Malek Salem',

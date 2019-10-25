3492a35bcb41a6a3886830ce307b48d817c550a7
Louay Hamada <loauy.hamada@gmail.com>
2019-10-25 23:55:24 +0300

    Get Latest Top 12 Topics

--- src/index.js
+++ src/index.js
@@ -1,55 +1,28 @@
-const ARTICLES_DEP =
-  "https://forums.coretabs.net/c/articles/l/top/monthly.json";
-const QUESTIONS_DEP =
-  "https://forums.coretabs.net/c/questions/l/top/monthly.json";
-
-// Manual Added Topics (Eg, Important Topics & Announcements From Coretabs Forums)
-const manualAddedTopics = [
-  {
-    author: {
-      name: 'مجتمع كورتابز',
-      profile_url: 'https://coretabs.net',
-      avatar_url: 'https://forums.coretabs.net/uploads/default/original/1X/7aff4233bdc45b6505ba9ea00ef45e323b569e6d.png'
-    },
-    title: 'سلسلة التحدي الأسبوعي: التحدي 1',
-    topic_url: 'https://forums.coretabs.net/t/%D8%B3%D9%84%D8%B3%D9%84%D8%A9-%D8%A7%D9%84%D8%AA%D8%AD%D8%AF%D9%8A-%D8%A7%D9%84%D8%A3%D8%B3%D8%A8%D9%88%D8%B9%D9%8A-%D8%A7%D9%84%D8%AA%D8%AD%D8%AF%D9%8A-1/3151',
-    pic: 'https://img.freepik.com/free-vector/trophie_53876-25485.jpg?size=338&ext=jpg'
-  }
-];
-
-let topics = [];
-
-let articlePosts = fetchTopics(ARTICLES_DEP).then(data => {
+let topTopics = fetchTopics("https://forums.coretabs.net/top/monthly.json").then(data => {
+  // Sort Topics by date
   let sortedArray = sortTopics(data.topics);
-  let articlesTopics = sortedArray.slice(0, 10);
+  
+  // Filter Topics by 12, 16 and 19 Categories
+  let categorizedTopics = sortedArray.filter(function(topic) {
+    return topic.category_id === 12 || topic.category_id === 16 || topic.category_id === 19
+  });
+
+  // Get First 12 Topics 
+  let latestTopics = categorizedTopics.slice(0, 12);
 
   // Store All authors in this department to filter it out later
   let authors = data.authors;
 
   // Put author details in topic
-  articlesTopics.map((topic, index) => {
-    return (topic.author = authors.filter(author => {
-      return author.id === topic.posters[0].user_id;
-    })[0]);
+  latestTopics.map(topic => {
+    let author = authors.find(author => { return author.id === topic.posters[0].user_id; });
+    author.avatar_url = `https://forums.coretabs.net/${author.avatar_template.replace(/{.*}/, 45)}`;
+    author.profile_url = `https://forums.coretabs.net/u/${author.username}`;
+    
+    return topic.author = author;
   });
-
-  // Merge previous & new topics together and sort them
-  return articlesTopics;
-});
-
-let questions = fetchTopics(QUESTIONS_DEP).then(data => {
-  let sortedArray = sortTopics(data.topics);
-  let qustionsTopics = sortedArray.slice(0, 5);
-  // Store All authors in this department to filter it out later
-  let authors = data.authors;
-  // Put author details in topic
-  qustionsTopics.map((topic, index) => {
-    return (topic.author = authors.filter(author => {
-      return author.id === topic.posters[0].user_id;
-    })[0]);
-  });
-  // Merge previous & new topics together and sort them
-  return qustionsTopics;
+  
+  return latestTopics;
 });
 
 async function fetchTopics(departmentUrl) {
@@ -64,65 +37,49 @@ function sortTopics(topics) {
   });
 }
 
-Promise.all([articlePosts, questions])
-  .then(topics => {
-    topics = topics[0].concat(topics[1]);
-    for (let topic of topics) {
-      let topicUrl = `https://forums.coretabs.net/t/${topic.slug}/${topic.id}`;
-      topic.title = topic.title;
-      topic.topic_url = topicUrl;
-      topic.pic = `https://source.unsplash.com/random/300x300?sig=${topic.id}&programming`;
-      topic.author = Object.assign({}, topic.author);
-      topic.author.avatar_url = `https://forums.coretabs.net/${topic.author.avatar_template.replace(
-        /{.*}/,
-        45
-      )}`;
-      topic.author.profile_url = `https://forums.coretabs.net/u/${topic.author.username}`;
-    }
+function truncateString(str, num) {
+  return str.length > num ? str.slice(0, num > 3 ? num - 3 : num) + "..." : str;
+}
 
-    // Merge manual added topics
-    topics = [...manualAddedTopics, ...topics];
+window.onload = async function() {
+  let list = document.querySelector(".articles-wrapper__carousel");
+  
+  const topics = await topTopics;
+  topics.forEach(function(article) {
+    let articleItemElement = document.createElement("article");
+    let articleTitle = truncateString(article.title, 60);
+    
+    articleItemElement.className = "article";
+    articleItemElement.innerHTML = `
+      <div class="article__meta">
+        <a href="${article.author.profile_url}" target="_parent">
+          <figure class="article__author-pic">
+            <img src="${article.author.avatar_url}" alt="${article.author.name}">    
+          </figure>
+        </a>
+      </div>
+      <a href="https://forums.coretabs.net/t/${article.slug}/${article.id}" class="article__link"  target="_parent">
+        <div class="article__body">
+          <p class="article__title">${articleTitle}</p>
+        </div>
+      </a>
+      <div class="article__meta">
+        <span class="article__author-name">${article.author.name}</span>
+      </div>
+      <div class="tint">
+        <img src="https://source.unsplash.com/random/300x300?sig=${article.id}&programming" alt="${articleTitle}"/>                
+      </div>
+    `;
 
-    return topics;
-  })
-  .then(articles => {
-    let list = document.querySelector(".articles-wrapper__carousel");
-    const truncateString = (str, num) =>
-      str.length > num ? str.slice(0, num > 3 ? num - 3 : num) + "..." : str;
-    for (let article of articles) {
-      let articleItemElement = document.createElement("article");
-      articleItemElement.className = "article";
-      let articleTitle = truncateString(article.title, 60);
-      let avatar_url = article.author.avatar_url;
-      articleItemElement.innerHTML = `
-            <div class="article__meta">
-                <a href="${article.author.profile_url}" target="_parent">
-                    <figure class="article__author-pic">
-                        <img src="${article.author.avatar_url}" alt="${article.author.name}">    
-                    </figure>
-                </a>
-            </div>
-            <a href="${article.topic_url}" class="article__link"  target="_parent">
-                <div class="article__body">
-                    <p class="article__title">${articleTitle}</p>
-                </div>
-            </a>
-            <div class="article__meta">
-                <span class="article__author-name">${article.author.name}</span>
-            </div>
-            <div class="tint">
-                <img src="${article.pic}" alt="${articleTitle}"/>                
-            </div>
-        `;
-      list.appendChild(articleItemElement);
-    }
-    var elem = document.querySelector(".articles-wrapper__carousel");
-    var flkty = new Flickity(elem, {
-      // options
-      cellAlign: "right",
-      contain: true,
-      freeScroll: true,
-      pageDots: false,
-      rightToLeft: true
-    });
+    list.appendChild(articleItemElement);
   });
+  
+  new Flickity(list, {
+    cellAlign: "right",
+    contain: true,
+    freeScroll: true,
+    pageDots: false,
+    rightToLeft: true
+  });
+};
+
